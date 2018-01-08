9f1e3528e7fba7a4fde81fb3eb107f2b4e8e7329
we0z <weoz@ukr.net>
2018-01-08 22:01:46 +0200

    Creational[Prototype - Demo-Prototype]

---
+++ src/main/java/design_patterns/creational_patterns/prototype/Movie.java
@@ -0,0 +1,13 @@
+package design_patterns.creational_patterns.prototype;
+
+public class Movie extends Item {
+    private String runtime;
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+}
