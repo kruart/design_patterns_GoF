de79d231c38798db1e621d6703aed6acc0c236cb
we0z <weoz@ukr.net>
2018-01-10 13:45:19 +0200

    Structural[Adapter - Demo-Adapter]

---
+++ src/main/java/design_patterns/structural_patterns/adapter/custom_example/Employee.java
@@ -0,0 +1,10 @@
+package design_patterns.structural_patterns.adapter.custom_example;
+
+public interface Employee {
+
+    String getId();
+    String getFirstName();
+    String getLastName();
+    String getEmail();
+
+}
---
+++ src/main/java/design_patterns/structural_patterns/adapter/custom_example/EmployeeClient.java
@@ -0,0 +1,20 @@
+package design_patterns.structural_patterns.adapter.custom_example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClient {
+
+    public List<Employee> getEmployeeList() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        Employee employeeFromDB = new EmployeeDB("1234", "John", "Wick", "john@wick.com");
+        employees.add(employeeFromDB);
+
+        EmployeeLdap employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");
+        employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+
+        return employees;
+    }
+}
