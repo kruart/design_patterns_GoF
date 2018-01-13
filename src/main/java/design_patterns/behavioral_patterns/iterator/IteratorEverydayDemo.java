ed78e1c7fb06eb006b1c26d8f7b275954456cc70
we0z <weoz@ukr.net>
2018-01-13 20:46:33 +0200

    Behavioral[Iterator - Demo-List]

---
+++ src/main/java/design_patterns/behavioral_patterns/iterator/IteratorEverydayDemo.java
@@ -0,0 +1,27 @@
+package design_patterns.behavioral_patterns.iterator;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class IteratorEverydayDemo {
+
+	public static void main(String[] args) {
+		//List<String> names = new ArrayList<>();
+		Set<String> names = new HashSet<>();
+
+		names.add("Bryan");
+		names.add("Aaron");
+		names.add("Jason");
+		
+		Iterator<String> namesItr = names.iterator();
+
+		while(namesItr.hasNext()) {
+			String name = namesItr.next();
+			System.out.println(name);
+			namesItr.remove();
+		}
+		
+		System.out.println("Names size: " + names.size());
+	}
+}
